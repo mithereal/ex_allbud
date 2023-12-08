58d8d03f8c4282abd1a9bb1bd1fd8fc95cb88883
mithereal <mithereal@gmail.com>
2023-12-08 16:43:12 -0700

    update hackeny

--- mix.exs
+++ mix.exs
@@ -29,8 +29,8 @@ defmodule Allbud.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:floki, ">= 0.30.0"},
-      {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.17"},
+      {:tesla, ">= 0.0.0"},
+      {:hackney, "~> 1.20"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
