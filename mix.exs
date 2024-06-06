46eabb9b6a8a434c5ea8c1e78c05bd3b77225ac7
mithereal <mithereal@gmail.com>
2024-06-06 13:40:14 -0700

    add coveralls

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule Allbud.MixProject do
   use Mix.Project
 
+  @version "0.1.1"
+  @source_url "https://github.com/mithereal/ex_allbud.git"
+
   def project do
     [
       app: :allbud,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -12,7 +15,8 @@ defmodule Allbud.MixProject do
       source_url: "https://github.com/mithereal/ex_allbud",
       description: "Scrape all strain information from allbud.com",
       docs: docs(),
-      package: package()
+      package: package(),
+      aliases: aliases()
     ]
   end
 
@@ -31,18 +35,44 @@ defmodule Allbud.MixProject do
       {:floki, ">= 0.30.0"},
       {:tesla, ">= 0.0.0"},
       {:hackney, "~> 1.20"},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.14", only: [:test, :dev]}
     ]
   end
 
   defp package do
     [
       licenses: ["MIT"],
+      files: ["lib", "priv", "mix.exs", "README.md"],
+      maintainers: ["Jason Clark"],
+      licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/mithereal/ex_allbud"}
+
     ]
   end
 
   defp docs do
-    []
+    [
+      main: "readme",
+      homepage_url: @source_url,
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp aliases do
+    [
+      c: "compile",
+      test: [
+        "ecto.drop --quiet",
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "run priv/seeds.exs",
+        "test"
+      ],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+    ]
   end
 end
